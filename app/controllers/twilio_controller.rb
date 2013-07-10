6df5062a6bf78ff7de940cd3944aa40fbafe13e0
Ben Cox <benjamin.jl.cox@gmail.com>
2013-07-10 13:07:24 -0400

    Testing fetching and sending of products over sms via twiml

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,6 +1,7 @@
 class TwilioController < ApplicationController
 
 	def process_sms
+		@products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
 		@city = params[:FromCity].capitalize
     	@state = params[:FromState]
 		render 'process_sms.xml.erb', :content_type => 'text/xml'
