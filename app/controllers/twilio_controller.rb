db87f235d62033901c6a73201fd4a1e21a961d79
Ben Cox <benjamin.jl.cox@gmail.com>
2013-07-10 13:21:17 -0400

    Adding more shopify stuff

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,6 +2,11 @@ class TwilioController < ApplicationController
 
 	 around_filter :shopify_session, :except => 'welcome' 
 
+	def welcome
+	    current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
+	    @callback_url = "http://#{current_host}/login/finalize"
+  	end
+
 	def process_sms
 		@products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
 		@city = params[:FromCity].capitalize
