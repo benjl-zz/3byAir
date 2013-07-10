5b5c090bfca8a7d5fc5b94f0e7660692384962fa
Ben Cox <benjamin.jl.cox@gmail.com>
2013-07-10 13:59:18 -0400

    shit.:

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,12 +1,5 @@
 class TwilioController < ApplicationController
 
-	 around_filter :shopify_session, :except => 'welcome' 
-
-	def welcome
-	    current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
-	    @callback_url = "http://#{current_host}/sms"
-  	end
-
 	def process_sms
 		@products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
 		@city = params[:FromCity].capitalize
