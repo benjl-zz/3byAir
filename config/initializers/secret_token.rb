ea268451339f0ef0af2277c3cc7c965a63e6efd3
Ben Cox <benjamin.jl.cox@gmail.com>
2013-07-10 09:57:12 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threebyair::Application.config.secret_token = '818042d034753e0e846c2dcf41e976acf3e07aaba0b2aa6d17b7453b3d7f88dd0d13dc97cab1a266a9d80ce8c80063c3136576148e5c315b4c76ef37c1dc04e8'
