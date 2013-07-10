3abaa85c21b1c3ff7223adc14b9c7954c69a3144
Ben Cox <benjamin.jl.cox@gmail.com>
2013-07-10 14:56:25 -0400

    Starting over again

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,14 +1,6 @@
 class TwilioController < ApplicationController
-	
-	around_filter :shopify_session, :except => 'welcome'
-  
-	def welcome
-	current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
-	@callback_url = "http://#{current_host}"
-	end
 
 	def process_sms
-		@products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
 		@city = params[:FromCity].capitalize
     	@state = params[:FromState]
 		render 'process_sms.xml.erb', :content_type => 'text/xml'
