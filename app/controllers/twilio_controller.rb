440604ae25fe8900f2cab77fbf39f460add1381b
Ben Cox <benjamin.jl.cox@gmail.com>
2013-07-10 13:12:25 -0400

    Forgot to include shopify filter

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,5 +1,7 @@
 class TwilioController < ApplicationController
 
+	 around_filter :shopify_session, :except => 'welcome' 
+
 	def process_sms
 		@products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
 		@city = params[:FromCity].capitalize
